9c1568ff5375d17d2993a26155c2bfc949d9a480
Luke Vandenbergh <luktut@gmail.com>
2014-06-14 00:20:03 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+inherits = require("inherits");
+var em = require('events').EventEmitter;
+
+module.exports = function(game, opts) {
+    return new Exp(game, opts);
+}
+
+function Exp(game, opts) {
+    this.curve = opts.curve || function(cl){return (cl) * 500};
+    this.minLevel = opts.minLevel || 1;
+    this.maxLevel = opts.maxLevel || 99;
+    this.cl = opts.startLevel || 1;
+    this.canDeLevel  = opts.canDeLevel || false;
+
+    if (this.cl != 1) {
+        this.cexp = this.curve(cl-1);
+        this.minExp = this.cexp
+    }
+    else {
+        this.cexp = 0;
+        this.minExp = 0;
+    }
+
+    this.game = game;
+    this.enable();
+}
+
+inherits(Exp, em);
+
+Exp.inc = function(v) {
+    this.cexp += v;
+    this.emit('exp', this.cexp);
+    this.emit('expgain', v);
+    this.maybeLevel();
+};
+
+Exp.dec = function(v) {
+    this.cexp -= v;
+    this.emit('exp', this.cexp);
+    this.emit('exploss', v);
+    this.maybeLevel();
+};
+
+Exp.maybeLevel = function() {
+    if (this.cexp >= this.curve(this.cl+1)) {
+        this.cl++;
+        this.minExp = this.curve(this.cl-1);
+        this.emit('levelup', this.cl);
+    }
+
+    if (this.canDeLevel && this.cexp < this.curve(this.cl-1)) {
+        this.cl--;
+        this.minExp = this.curve(this.cl-1);
+        this.emit('leveldown'. this.cl);
+    }
+};
+
+Exp.toNext = function() {
+    return this.curve(this.cl) - this.cexp;
+};
+
+Exp.precent = function() {
+    return this.cexp / this.curve(this.cl) * 100.0;
+};
---
+++ test.js
@@ -0,0 +1,49 @@
+var test = require('tape')
+var exp = require('./')
+
+function Game() {}
+
+function defaults(t) {
+    //Check defaults
+    var p = exp(new Game(), {});
+    t.equals(p.cexp, 0);
+    t.equals(p.minExp, 0);
+    t.equals(p.cl, 1);
+    t.equals(p.minLevel, 1);
+    t.equals(p.maxLevel, 99);
+    t.equals(p.curve(p.cl), 500);
+    t.equals(p.toNext(), 100);
+
+    t.end();
+};
+
+function inc(t) {
+    //Check Exp.inc
+    var p = exp(new Game(), {});
+    p.inc(500);
+    t.equals(p.cexp, 500);
+    t.equals(p.cl, 2);
+    t.equals(p.curve(p.cl), 1000);
+    p.inc(500);
+    t.equals(p.cexp, 1000);
+    t.equals(p.cl, 3);
+
+    t.end();
+};
+
+function dec(t) {
+    var p = exp(new Game(), {canDeLevel: true});
+    p.inc(1000);
+    t.equals(p.cexp, 1000);
+    p.dec(500);
+    t.equals(p.cexp, 500);
+    p.dec(1);
+    t.equals(p.cexp, 499);
+    t.equals(p.cl, 1);
+
+    t.end();
+}
+
+test('defaults', defaults);
+test('inc', inc);
+test('dec', dec);
